78e65e638f1474fd3060d7737941a5e2a29452a2
gabrielestefono <gabrielestefono@hotmail.com>
2023-06-27 21:33:19 -0300

    Buscando no BD

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 "use client"
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import styles from './page.module.scss';
 import { database } from '@/services/firebase';
+
+type Contato = {
+  chave: string,
+  nome: string,
+  email: string,
+  telefone: string,
+  observacoes: string
+}
 
 
 export default function Home() {
@@ -9,6 +17,21 @@ export default function Home() {
   const [email, setEmail] = useState('')
   const [telefone, setTelefone] = useState('')
   const [observacoes, setObservacoes] = useState('')
+
+  useEffect(()=>{
+    const refContatos = database.ref('contatos');
+    refContatos.on('value', resultado => {
+      const resultadoContatos = Object.entries<Contato>(resultado.val() ?? {}).map(([chave, valor])=>{
+        return{
+          'chave': chave,
+          'nome': valor.nome,
+          'email': valor.email,
+          'telefone': valor.telefone,
+          'observacoes': valor.observacoes
+        }
+      })
+    });
+  }, []);
 
   let gravar = (event: FormEvent)=>{
     event.preventDefault();
@@ -22,16 +45,21 @@ export default function Home() {
     };
 
     ref.push(dados);
+    setNome('');
+    setEmail('');
+    setTelefone('');
+    setObservacoes('');
+    
   }
 
   return (
     <>
       <main className={styles.container}>
           <form onSubmit={gravar}>
-            <input type="text" placeholder='Nome' onChange={event => setNome(event.target.value)}/>
-            <input type="email" placeholder='Email' onChange={event => setEmail(event.target.value)}/>
-            <input type="tel" placeholder='Telefone' onChange={event => setTelefone(event.target.value)}/>
-            <textarea name="observacao" placeholder='Observação' onChange={event => setObservacoes(event.target.value)}></textarea>
+            <input type="text" placeholder='Nome' value={nome} onChange={event => setNome(event.target.value)}/>
+            <input type="email" placeholder='Email' value={email} onChange={event => setEmail(event.target.value)}/>
+            <input type="tel" placeholder='Telefone' value={telefone} onChange={event => setTelefone(event.target.value)}/>
+            <textarea name="observacao" placeholder='Observação' value={observacoes} onChange={event => setObservacoes(event.target.value)}></textarea>
             <button type='submit'>Salvar</button>
           </form>
           <div className={styles.caixacontatos}>
